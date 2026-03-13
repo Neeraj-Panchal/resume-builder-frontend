88165b49adf8f0c4c2931eb6f51213b462393e4a
Neeraj Panchal <neerajpanchal098@gmail.com>
2026-03-13 17:48:40 +0530

    axios config fix: if condition updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -11,7 +11,7 @@ const api = axios.create({
 // Request Interceptor (Pehle jaisa hi rahega)
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
-    if (token) {
+    if (token && token !== 'null' && token !== 'undefined') {
         config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
